bcfc23ee7a6ac76e04d31fb2aec0df1aa00ef8c8
Bunyodbek Nurmamatov <bunyodbeknurmamatov@gmail.com>
2025-02-10 19:32:08 +0500

    HttpEntity

--- src/main/java/org/example/clickup/controller/UserController.java
+++ src/main/java/org/example/clickup/controller/UserController.java
@@ -5,6 +5,9 @@ import org.example.clickup.model.Result;
 import org.example.clickup.model.User;
 import org.example.clickup.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,31 +19,33 @@ public class UserController {
     UserService userService;
 
     @GetMapping
-    public List<User> getAll(){
+    public HttpEntity<?> getAll() {
         List<User> list = userService.getAll();
-        return list;
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public User getById(@PathVariable Integer id){
+    public HttpEntity<?> getById(@PathVariable Integer id) {
         User user = userService.getById(id);
-        return user;
+        return ResponseEntity.status(200).body(user);
     }
 
     @PostMapping
-    public Result create(@RequestBody UserDto userDto){
+    public HttpEntity<?> create(@RequestBody UserDto userDto) {
         Result result = userService.create(userDto);
-        return result;
+
+        //return new ResponseEntity<>(result,HttpStatus.CREATED);
+        return ResponseEntity.status(201).body(result);
     }
 
     @PutMapping("/{id}")
-    public Result update(@PathVariable Integer id, @RequestBody UserDto userDto){
+    public Result update(@PathVariable Integer id, @RequestBody UserDto userDto) {
         Result result = userService.update(id, userDto);
         return result;
     }
 
     @DeleteMapping("/{id}")
-    public Result delete(@PathVariable Integer id){
+    public Result delete(@PathVariable Integer id) {
         Result result = userService.delete(id);
         return result;
     }
